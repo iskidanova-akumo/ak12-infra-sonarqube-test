48e5a7abeff44efba068ed3dc34f32c25ef017de
iskidanova-akumo <iskidanova@akumoproject.com>
2025-08-07 18:30:02 -0500

    feat: added hardcoded password to python file

--- scripts/python.py
+++ scripts/python.py
@@ -18,16 +18,9 @@ subprocess.run(["ls", "-la"])
 
 API_SECRET_KEY = "mySuperSecretKey_aB1c2D3e4F5g6H7i"
 
-def get_discount(user_type):
-    if user_type == "regular":
-        return 5
-    elif user_type == "member":
-        return 10
-    elif user_type == "premium":
-        return 15
-    elif user_type == "vip":
-        return 20
-    elif user_type == "employee":
-        return 25
-    else:
-        return 0
+
+
+def check_password(password):
+    if password == "mypassword123":
+        return True
+    return False
