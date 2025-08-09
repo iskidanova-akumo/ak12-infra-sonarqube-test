8378d1dd24e206f94a92c28dbf0b51ccb7e7188c
iskidanova-akumo <iskidanova@akumoproject.com>
2025-08-09 11:29:31 -0500

    feat: changed the python file by adding some errors,v5

--- scripts/python.py
+++ scripts/python.py
@@ -18,14 +18,6 @@ subprocess.run(["ls", "-la"])
 
 API_SECRET_KEY = "mySuperSecretKey_aB1c2D3e4F5g6H7i"
 
-def get_discount(customer_type):
-    if customer_type == 'silver':
-        return 0.15
-    elif customer_type == 'gold':
-        return 0.15                             #Identical to previous branch
-    else:
-        return 0.05
-
 
 secret_password = "Python123"
 
@@ -35,6 +27,8 @@ for i in range(3):
     else:
         print("Unable to access")
 
-def celsius_to_fahrenheit(c):
-    return (c - 32) * 5/9
 
+def check_value(value):
+    if value < 0:
+        raise BaseException("Value cannot be negative")
+        #The BaseException class is reserved for system-level exceptions
